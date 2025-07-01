755e4ad7206a75e8a145d30cf3e3d864c5b60ba4
davidschwarzer <schwarzerdavid@gmail.com>
2025-07-01 21:45:36 +0300

    register blueprints

--- App/__init__.py
+++ App/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 
-app = Flask(__name__)
+from App.src.placeholder1.placeholder1_routes import placeholder1
+from App.src.placeholder2.placeholder2_routes import placeholder2
 
-from App.src import routes
+app = Flask(__name__)
+app.register_blueprint(placeholder1)
+app.register_blueprint(placeholder2)
