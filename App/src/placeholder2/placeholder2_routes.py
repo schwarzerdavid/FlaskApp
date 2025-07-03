9b187bb8fec2d2c6edfd19ccc450ae5ab4466041
davidschwarzer <schwarzerdavid@gmail.com>
2025-07-03 07:39:54 +0300

    frefactoring + docstrings

--- App/src/placeholder2/placeholder2_routes.py
+++ App/src/placeholder2/placeholder2_routes.py
@@ -4,9 +4,9 @@ from flask_restx import Api, Resource
 placeholder2 = Blueprint('placeholder2', __name__, url_prefix='/placeholder2')
 api = Api(placeholder2)
 placeholder2_ns = api.namespace('placeholder2', description='Placeholder 2')
-api.add_namespace(placeholder2_ns)
 
 @placeholder2_ns.route('/')
 class Placeholder2Root(Resource):
     def get(self):
-        return {"placeholder2": "placeholder2"}
+        "placeholder 2 documentation"
+        return {"placeholder2": "placeholder2"}, 200
